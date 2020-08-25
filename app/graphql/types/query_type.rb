1360d9ce548866c5cbb5c9d994c780b5ceed45bc
Gladys Chacartegui <gladys@codegram.com>
2020-08-25 10:35:08 +0200

    Add client type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module Types
+  # QueryType = GraphQL::ObjectType.new.tap do |root_type|
+  #   root_type.name = 'Query'
+  #   root_type.description = 'The query root'
+  #   root_type.interfaces = []
+  # end
+
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
