934b9254eb7f5c71871c61b9dd69f50442180679
Gladys Chacartegui <gladys@codegram.com>
2020-09-16 09:45:13 +0200

    Change faker public key to a RSA format

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :authority do
     name { Faker::Name.unique.name }
-    public_key { Faker::Internet.password }
+    public_key { OpenSSL::PKey::RSA.new(1024).public_key.to_s }
     api_key { Faker::Internet.unique.uuid }
   end
 end
