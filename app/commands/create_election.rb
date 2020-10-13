a68cf4b25cd4f18389189fbcbcdba360843a39a1
Gladys Chacartegui <gladys@codegram.com>
2020-10-13 21:40:05 +0200

    Fix problem with where clause for Trustees creation

--- app/commands/create_election.rb
+++ app/commands/create_election.rb
@@ -88,7 +88,7 @@ class CreateElection < Rectify::Command
       public_key: trustee_public_key(trustee),
       api_key: trustee_public_key(trustee)
     }
-    t = Trustee.where(name: trustee_name(trustee)).or(Trustee.where(public_key: trustee_public_key(trustee))).or(Trustee.where(api_key: trustee_public_key(trustee)))
+    t = Trustee.where(name: trustee_name(trustee)).or(Trustee.where(public_key: trustee_public_key(trustee))).or(Trustee.where(api_key: trustee_public_key(trustee))).first
     t = Trustee.create!(trustee_attributes) if t.blank?
     t.elections_trustees.create!(election: election)
   end
