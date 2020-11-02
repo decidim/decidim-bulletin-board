67698921ab5c54e68602089d64bcc2cf3d22af61
Leonardo Diez <leiodd@gmail.com>
2020-11-02 13:06:35 +0000

    test: complete rake task tests

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :authority do
-    name { Faker::Name.unique.name }
-    public_key { OpenSSL::PKey::RSA.new(1024).public_key.to_s }
-    api_key { Faker::Internet.unique.uuid }
-  end
-end
