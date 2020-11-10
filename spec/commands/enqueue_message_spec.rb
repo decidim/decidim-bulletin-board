db88bc97ad8f1c6e8762eb005b11b270a27b053e
Leonardo Diez <leiodd@gmail.com>
2020-11-10 17:22:37 +0000

    test: add key_ceremony tests

---
+++ spec/commands/enqueue_message_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe EnqueueMessage do
+  subject { described_class.call(client, signed_data, job) }
+
+  let(:client) { create(:trustee) }
+  let(:signed_data) { "SOME SIGNED DATA THAT WILL NOT BE VALIDATED" }
+  let(:job) { KeyCeremonyJob }
+
+  it "broadcasts ok" do
+    expect { subject }.to broadcast(:ok)
+  end
+
+  it "adds the message to the pending messages table" do
+    expect { subject }.to change(PendingMessage, :count).by(1)
+  end
+
+  it "enqueues the job to process the message", :jobs do
+    subject
+    expect(job).to have_been_enqueued
+  end
+end
