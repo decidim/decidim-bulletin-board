7f3041e29f97d1b9f276c4d2174a950b9c689afb
Leonardo Diez <leiodd@gmail.com>
2020-11-10 17:17:34 +0000

    feat: add key_ceremony mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_election, mutation: Mutations::CreateElectionMutation
+    field :key_ceremony, mutation: Mutations::KeyCeremonyMutation
   end
 end
