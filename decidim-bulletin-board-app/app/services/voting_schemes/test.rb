4f0b3a53af2a4e9352e0b3c3807d2385d807c9c0
Leonardo Diez <leiodd@gmail.com>
2020-11-12 12:54:59 +0100

    fix: delete files created by mistake by git

--- decidim-bulletin-board-app/app/services/voting_schemes/test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module VotingSchemes
-  class Test < Base
-    def validate_election
-      "There must be at least 2 Trustees" if election.manifest.fetch("trustees", []).count < 2
-    end
-  end
-end
