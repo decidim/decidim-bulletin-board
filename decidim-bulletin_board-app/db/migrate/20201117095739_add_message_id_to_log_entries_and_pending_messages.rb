fee16eb06f89cbd3f0204d351c50e2527fbe7656
Leonardo Diez <leiodd@gmail.com>
2020-11-18 11:33:26 +0000

    style: fix lint issues

--- decidim-bulletin_board-app/db/migrate/20201117095739_add_message_id_to_log_entries_and_pending_messages.rb
+++ decidim-bulletin_board-app/db/migrate/20201117095739_add_message_id_to_log_entries_and_pending_messages.rb
@@ -4,8 +4,8 @@ class PendingMessage < ApplicationRecord; end
 
 class AddMessageIdToLogEntriesAndPendingMessages < ActiveRecord::Migration[6.0]
   def change
-    rename_column :log_entries, :log_type, :message_id
     add_column :pending_messages, :message_id, :string, null: true
+    rename_column :log_entries, :log_type, :message_id
 
     PendingMessage.all.each { |pending_message| pending_message.update_attribute(:message_id, "") } # rubocop:disable Rails/SkipsModelValidations
 
