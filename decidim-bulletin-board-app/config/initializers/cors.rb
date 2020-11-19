684754332afdd959311ea207c807f89421cb7bce
David Morcillo <david@codegram.com>
2020-11-19 16:49:35 +0100

    fix: only allow the configured domain

--- decidim-bulletin-board-app/config/initializers/cors.rb
+++ decidim-bulletin-board-app/config/initializers/cors.rb
@@ -4,7 +4,7 @@ return if Rails.application.config.settings.cors_origin_allowed.blank?
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins Rails.application.config.settings.cors_origin_allowed
     resource "*", headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
