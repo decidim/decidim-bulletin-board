ac3fa3b1d767fd10f349607a6eefa668e1520612
David Morcillo <david@codegram.com>
2020-12-08 15:52:15 +0100

    feat: cast vote from the ruby client (#30)

--- decidim-bulletin_board-ruby/lib/decidim/bulletin_board.rb
+++ decidim-bulletin_board-ruby/lib/decidim/bulletin_board.rb
@@ -2,10 +2,13 @@
 
 require "decidim/bulletin_board/version"
 require "graphlient"
+require "wisper"
+require "active_model"
 require "decidim/bulletin_board/jwk_utils"
 require "decidim/bulletin_board/client"
 require "decidim/bulletin_board/graphql/client"
 require "decidim/bulletin_board/create_election"
+require "decidim/bulletin_board/voter"
 require "active_support/configurable"
 require "jwt"
 
