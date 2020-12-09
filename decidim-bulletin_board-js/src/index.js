e8d1dcba1d1f945cb613fea06d97c29e2f35b8cf
David Morcillo <david@codegram.com>
2020-12-09 17:15:18 +0100

    fix(js-client): export Voter and return dummy encrypted vote as JSON

--- decidim-bulletin_board-js/src/index.js
+++ decidim-bulletin_board-js/src/index.js
@@ -1,4 +1,5 @@
 import { Client } from "./client/client";
 import { KeyCeremony } from "./key-ceremony/key-ceremony";
+import { Voter } from "./voter/voter";
 
-export { Client, KeyCeremony };
+export { Client, KeyCeremony, Voter };
