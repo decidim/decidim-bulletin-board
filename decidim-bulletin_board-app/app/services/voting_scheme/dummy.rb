8c6312f1539502a5b3f390334756a843afe1eebd
Leonardo Diez <leiodd@gmail.com>
2020-12-10 16:18:28 +0000

    feat: use separated content for the dummy scheme

--- decidim-bulletin_board-app/app/services/voting_scheme/dummy.rb
+++ decidim-bulletin_board-app/app/services/voting_scheme/dummy.rb
@@ -7,9 +7,10 @@ module VotingScheme
   class Dummy < Base
     def process_message(message_identifier, message)
       method_name = :"process_#{message_identifier.type}_message"
+      content = JSON.parse(message.delete(:content) || "null")&.with_indifferent_access
       if respond_to?(method_name, true)
         @response = nil
-        method(method_name).call(message)
+        method(method_name).call(message, content)
         @response
       end
     end
@@ -20,18 +21,18 @@ module VotingScheme
       @response = response
     end
 
-    def process_create_election_message(message)
-      raise RejectedMessage, "There must be at least 2 Trustees" if message.fetch("trustees", []).count < 2
+    def process_create_election_message(message, content)
+      raise RejectedMessage, "There must be at least 2 Trustees" if message.fetch(:trustees, []).count < 2
 
       @state = { joint_election_key: 1, trustees: [] }
     end
 
-    def process_key_ceremony_message(message)
-      election_public_key = message.fetch("election_public_key", 0)
-      raise RejectedMessage, "The trustee sent their public keys already" if state[:trustees].include?(message["owner_id"])
+    def process_key_ceremony_message(message, content)
+      election_public_key = content.fetch(:election_public_key, 0)
+      raise RejectedMessage, "The trustee sent their public keys already" if state[:trustees].include?(content[:owner_id])
       raise RejectedMessage, "The election public key should be a prime number" unless Prime.prime?(election_public_key)
 
-      state[:trustees] << message["owner_id"]
+      state[:trustees] << content[:owner_id]
       state[:joint_election_key] *= election_public_key
 
       if state[:trustees].count == election.trustees.count
@@ -41,8 +42,8 @@ module VotingScheme
       end
     end
 
-    def process_vote_message(message)
-      raise RejectedMessage, "The given ballot style is invalid" if message.fetch("ballot_style", "invalid-style") == "invalid-style"
+    def process_vote_message(message, content)
+      raise RejectedMessage, "The given ballot style is invalid" if content.fetch(:ballot_style, "invalid-style") == "invalid-style"
     end
   end
 end
