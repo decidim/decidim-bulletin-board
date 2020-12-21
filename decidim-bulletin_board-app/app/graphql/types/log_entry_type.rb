d61db323ceea6ce64a02014c9ae721e2c27c4cde
Leonardo Diez <leiodd@gmail.com>
2020-12-21 12:28:59 +0000

    fix: allow content_hash to be nil in graphql

--- decidim-bulletin_board-app/app/graphql/types/log_entry_type.rb
+++ decidim-bulletin_board-app/app/graphql/types/log_entry_type.rb
@@ -7,7 +7,7 @@ module Types
     field :client, Types::ClientType, null: false
     field :signed_data, String, null: false
     field :chained_hash, String, null: false
-    field :content_hash, String, null: false
+    field :content_hash, String, null: true
     field :message_id, String, null: false
   end
 end
