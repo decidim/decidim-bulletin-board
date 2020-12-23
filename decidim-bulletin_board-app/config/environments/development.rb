0319278579609fa112396daa2f6501f391a136c7
leio10 <leiodd@gmail.com>
2020-12-23 19:39:13 +0100

    Support for request only the last log entries of an election (#58)
    * chore: allow the dev server to be on *.lvh.me
    
    * feat: allow to get only the last log entries for an election

--- decidim-bulletin_board-app/config/environments/development.rb
+++ decidim-bulletin_board-app/config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+
+  # Allow accessing with .lvh.me URLs
+  config.hosts << /[a-z0-9-]+\.lvh\.me/
 end
