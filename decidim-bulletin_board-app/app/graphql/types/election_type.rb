b7b2e5b7c3802ad8c2c17d62720ede0a49ea082d
Leonardo Diez <leiodd@gmail.com>
2020-12-23 18:34:46 +0000

    feat: allow to get only the last log entries for an election

--- decidim-bulletin_board-app/app/graphql/types/election_type.rb
+++ decidim-bulletin_board-app/app/graphql/types/election_type.rb
@@ -6,7 +6,18 @@ module Types
     field :title, String, null: false
     field :status, String, null: false
     field :authority, Types::ClientType, null: false
-    field :log_entries, [Types::LogEntryType], null: false, description: "Returns the list of log entries for this election in the bulletin board"
     field :trustees, [Types::ClientType], null: false, description: "Returns the list of trustees for this election"
+
+    field :log_entries, [Types::LogEntryType], null: false, description: "Returns the list of log entries for this election in the bulletin board" do
+      argument :after, String, required: false
+
+      def resolve(parent, args, _context)
+        if args[:after]
+          parent.object.log_entries.where("id > ?", args[:after].to_i)
+        else
+          parent.object.log_entries
+        end
+      end
+    end
   end
 end
