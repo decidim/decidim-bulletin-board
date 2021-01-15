8c67ede5768a3408862db36bdbc84daede325bca
Leonardo Diez <leiodd@gmail.com>
2021-01-15 17:15:44 +0000

    style: fix lint issues

--- decidim-bulletin_board-app/db/seeds.rb
+++ decidim-bulletin_board-app/db/seeds.rb
@@ -8,14 +8,14 @@ $LOAD_PATH.push base_path
 require "factory_bot_rails"
 require "dev_private_keys"
 
-dev_authority = Authority.create!(
+Authority.create!(
   name: "Decidim Test Authority",
   public_key: DevPrivateKeys.authority_public_key,
   public_key_thumbprint: Decidim::BulletinBoard::JwkUtils.thumbprint(DevPrivateKeys.authority_public_key),
   api_key: "89Ht70GZNcicu8WEyagz_rRae6brbqZAGuBEICYBCii-PTV3MAstAtx1aRVe5H5YfODi-JgYPvyf9ZMH7tOeZ15e3mf9B2Ymgw7eknvBFMRP213YFGo1SPn_C4uLK90G"
 )
 
-dev_trustees = DevPrivateKeys.trustees_public_keys.each_with_index.map do |trustee_public_key, index|
+DevPrivateKeys.trustees_public_keys.each_with_index.map do |trustee_public_key, index|
   Trustee.create!(
     name: "Decidim Test Trustee #{index + 1}",
     public_key: trustee_public_key,
