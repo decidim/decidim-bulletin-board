8148569cdd76a3d1a4e701a6d762dee966e094f6
Leonardo Diez <leiodd@gmail.com>
2021-01-19 12:07:06 +0000

    feat: add tally to voting schemes

--- decidim-bulletin_board-app/app/services/voting_scheme/election_guard.rb
+++ decidim-bulletin_board-app/app/services/voting_scheme/election_guard.rb
@@ -10,6 +10,8 @@ module VotingScheme
     delegate :restore, :backup, to: :state
 
     def process_message(message_identifier, message)
+      return tally_cast if tally_start?(message_identifier)
+
       state.process_message(message_identifier.subtype || message_identifier.type, PyCall::Dict.new(message))
     end
 
@@ -17,6 +19,18 @@ module VotingScheme
 
     def state
       @state ||= EGBB.BulletinBoard.new
+    end
+
+    def tally_start?(message_identifier)
+      message_identifier.type == "tally" && message_identifier.subtype == "start"
+    end
+
+    def tally_cast
+      votes.each do |log_entry|
+        state.add_ballot(PyCall::Dict.new(log_entry.decoded_data[:content]))
+      end
+
+      state.get_tally_cast
     end
   end
 end
