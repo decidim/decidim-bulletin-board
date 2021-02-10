46162d0f35a949653493be5f24d61359f3f85b23
slickepinne <svenjaschaefer@gmail.com>
2021-02-10 19:22:04 +0100

    fix: end to end test

--- decidim-bulletin_board-app/spec/cypress/integration/sandbox/election.page.js
+++ decidim-bulletin_board-app/spec/cypress/integration/sandbox/election.page.js
@@ -146,7 +146,8 @@ export class ElectionPage {
    */
   castVote() {
     cy.findByText("Vote").click();
-    cy.findByText("Cast vote").should("not.be.disabled").click();
+    cy.findByText("Encrypt vote").click();
+    cy.findByText("Cast vote").should("be.visible").click();
   }
 
   /**
@@ -156,6 +157,29 @@ export class ElectionPage {
    */
   assertVoteHasBeenCasted() {
     cy.findByText("Your vote has been casted successfully").should(
+      "be.visible"
+    );
+    cy.findByText("Back").click();
+  }
+
+  /**
+   * Audit a vote.
+   *
+   * @returns {undefined}
+   */
+  auditVote() {
+    cy.findByText("Vote").click();
+    cy.findByText("Encrypt vote").click();
+    cy.findByText("Audit vote").should("be.visible").click();
+  }
+
+  /**
+   * Assert that the vote has been audited successfully.
+   *
+   * @returns {undefined}
+   */
+  assertVoteHasBeenAudited() {
+    cy.findByText("Your vote has been audited successfully").should(
       "be.visible"
     );
     cy.findByText("Back").click();
