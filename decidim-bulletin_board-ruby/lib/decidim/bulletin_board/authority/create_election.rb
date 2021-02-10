604fd62f763b7042bd67be971f65fd81e3d7d48b
Leonardo Diez <leiodd@gmail.com>
2021-02-10 09:28:10 +0000

    refactor: build create_election message in the gem

--- decidim-bulletin_board-ruby/lib/decidim/bulletin_board/authority/create_election.rb
+++ decidim-bulletin_board-ruby/lib/decidim/bulletin_board/authority/create_election.rb
@@ -19,10 +19,10 @@ module Decidim
           # arguments used inside the graphql operation
           args = {
             message_id: message_id,
-            signed_data: sign_message(message_id, election_data)
+            signed_data: sign_message(message_id, message)
           }
 
-          response = client.query do
+          response = graphql.query do
             mutation do
               createElection(messageId: args[:message_id], signedData: args[:signed_data]) do
                 election do
@@ -43,6 +43,101 @@ module Decidim
         private
 
         attr_reader :election_data, :election_id
+
+        def message
+          {
+            scheme: scheme,
+            bulletin_board: bulletin_board,
+            authority: authority,
+            trustees: trustees,
+            description: {
+              name: text(election_data[:title]),
+              start_date: election_data[:start_date].iso8601,
+              end_date: election_data[:end_date].iso8601,
+              candidates: candidates,
+              contests: contests
+            }
+          }
+        end
+
+        def scheme
+          {
+            name: settings.scheme_name,
+            quorum: settings.quorum
+          }
+        end
+
+        def bulletin_board
+          {
+            public_key: settings.server_public_key
+          }
+        end
+
+        def authority
+          {
+            name: settings.authority_name,
+            public_key: settings.public_key
+          }
+        end
+
+        def trustees
+          election_data[:trustees].map do |trustee|
+            {
+              name: trustee[:name],
+              public_key: trustee[:public_key]
+            }
+          end
+        end
+
+        def contests
+          election_data[:questions].map do |question|
+            {
+              "@type": "CandidateContest",
+              object_id: question[:slug],
+              sequence_order: question[:weight],
+              vote_variation: question[:max_selections] == 1 ? "one_of_m" : "n_of_m",
+              name: default_text(question[:title]),
+              number_elected: question[:answers].count,
+              votes_allowed: 1,
+              ballot_title: text(question[:title]),
+              ballot_subtitle: text(question[:description]),
+              ballot_selections: contest_answers(question)
+            }
+          end
+        end
+
+        def contest_answers(question)
+          question[:answers].map do |answer|
+            {
+              object_id: "#{question[:slug]}_#{answer[:slug]}",
+              sequence_order: answer[:weight],
+              candidate_id: answer[:slug]
+            }
+          end
+        end
+
+        def candidates
+          election_data[:answers].map do |answer|
+            {
+              object_id: answer[:slug],
+              ballot_name: text(answer[:title])
+            }
+          end
+        end
+
+        def default_text(field)
+          field[default_locale]
+        end
+
+        def text(field)
+          {
+            text: field.map { |locale, value| { language: locale.to_s, value: value } }
+          }
+        end
+
+        def default_locale
+          @default_locale ||= election_data[:default_locale].to_sym
+        end
       end
     end
   end
