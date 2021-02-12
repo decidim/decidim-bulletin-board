9a62d268f92374bd2966af0b704b9abd9b08c45e
David Morcillo <david@codegram.com>
2021-02-12 14:36:33 +0100

    fix: lint errors

--- decidim-bulletin_board-app/app/javascript/packs/sandbox/vote.js
+++ decidim-bulletin_board-app/app/javascript/packs/sandbox/vote.js
@@ -67,9 +67,9 @@ $(async () => {
       $castVote.on("click", onEventTriggered);
     },
     onVoteAudition(auditedVote, auditFileName) {
-      vote = JSON.stringify(auditedVote);
+      const vote = JSON.stringify(auditedVote);
+      const link = document.createElement("a");
 
-      let link = document.createElement("a");
       link.setAttribute("href", `data:text/plain;charset=utf-8,${vote}`);
       link.setAttribute("download", auditFileName);
       document.body.appendChild(link);
