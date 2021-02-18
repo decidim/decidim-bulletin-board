c57d973fe8bef79cdd4508657ab848e16198e563
David Morcillo <david@codegram.com>
2021-02-18 17:34:28 +0100

    feat: add the VoterWrapperAdapter to the dummy scheme

---
+++ voting-scheme-dummy/src/voter_wrapper.test.js
@@ -0,0 +1,155 @@
+import {
+  VoterWrapper,
+  CREATE_ELECTION,
+  END_KEY_CEREMONY,
+} from "./voter_wrapper";
+
+const contestsExample = [
+  {
+    object_id: "question-1",
+    ballot_selections: [
+      {
+        object_id: "question-1-answer-1",
+      },
+      {
+        object_id: "question-1-answer-2",
+      },
+    ],
+  },
+  {
+    object_id: "question-2",
+    ballot_selections: [
+      {
+        object_id: "question-2-answer-1",
+      },
+      {
+        object_id: "question-2-answer-2",
+      },
+    ],
+  },
+];
+
+describe("VoterWrapper", () => {
+  const defaultParams = {
+    voterId: "voter-1",
+  };
+
+  const buildVoterWrapper = (params = defaultParams) => {
+    return new VoterWrapper(params);
+  };
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = buildVoterWrapper();
+  });
+
+  it("initialises the trustee wrapper with the given params", () => {
+    expect(wrapper.voterId).toEqual(defaultParams.voterId);
+    expect(wrapper.jointElectionKey).toEqual(null);
+    expect(wrapper.contests).toEqual({});
+  });
+
+  describe("processMessage", () => {
+    describe("when it receives the CREATE_ELECTION message", () => {
+      it("changes the wrapper status and stores some data", () => {
+        wrapper.processMessage(CREATE_ELECTION, {
+          description: {
+            contests: contestsExample,
+          },
+        });
+
+        expect(wrapper.contests).toEqual(contestsExample);
+        expect(wrapper.jointElectionKey).toEqual(null);
+      });
+    });
+
+    describe("when it receives the END_KEY_CEREMONY message", () => {
+      it("changes the wrapper status and stores some data", () => {
+        wrapper.processMessage(END_KEY_CEREMONY, {
+          content: JSON.stringify({
+            joint_election_key: 123456789,
+          }),
+        });
+
+        expect(wrapper.jointElectionKey).toEqual(123456789);
+      });
+    });
+  });
+
+  describe("encrypt", () => {
+    beforeEach(() => {
+      wrapper.contests = contestsExample;
+      wrapper.jointElectionKey = 123456789;
+      jest.spyOn(Math, "random").mockImplementation(() => 0.5);
+    });
+
+    describe("when it receives a valid vote", () => {
+      it("returns a JSON with the encrypted ballot", async () => {
+        const response = await wrapper.encrypt({
+          "question-1": ["question-1-answer-1"],
+          "question-2": ["question-2-answer-2"],
+        });
+
+        expect(response.encryptedBallot).toEqual(
+          `{"ballot_style":"ballot-style","contests":[{"object_id":"question-1","ballot_selections":[{"object_id":"question-1-answer-1","ciphertext":${
+            250 * 123456789 + 1
+          }},{"object_id":"question-1-answer-2","ciphertext":${
+            250 * 123456789
+          }}]},{"object_id":"question-2","ballot_selections":[{"object_id":"question-2-answer-1","ciphertext":${
+            250 * 123456789
+          }},{"object_id":"question-2-answer-2","ciphertext":${
+            250 * 123456789 + 1
+          }}]}]}`
+        );
+      });
+
+      it("returns an Object with the audited ballot", async () => {
+        const response = await wrapper.encrypt({
+          "question-1": ["question-1-answer-1"],
+          "question-2": ["question-2-answer-2"],
+        });
+
+        expect(response.auditableBallot).toEqual({
+          ballot_style: "ballot-style",
+          contests: [
+            {
+              ballot_selections: [
+                {
+                  ciphertext: 30864197251,
+                  object_id: "question-1-answer-1",
+                  plaintext: 1,
+                  random: 0.5,
+                },
+                {
+                  ciphertext: 30864197250,
+                  object_id: "question-1-answer-2",
+                  plaintext: 0,
+                  random: 0.5,
+                },
+              ],
+              object_id: "question-1",
+            },
+            {
+              ballot_selections: [
+                {
+                  ciphertext: 30864197250,
+                  object_id: "question-2-answer-1",
+                  plaintext: 0,
+                  random: 0.5,
+                },
+                {
+                  ciphertext: 30864197251,
+                  object_id: "question-2-answer-2",
+                  plaintext: 1,
+                  random: 0.5,
+                },
+              ],
+              object_id: "question-2",
+            },
+          ],
+        });
+      });
+    });
+  });
+});
---
+++ voting-scheme-dummy/src/voter_wrapper_adapter.js
@@ -0,0 +1,43 @@
+import { VoterWrapper } from "./voter_wrapper";
+
+/**
+ * This is just a dummy implementation of a possible `VoterWrapperAdapter`.
+ * It is based on the dummy voting scheme that we are using in the Bulletin Board.
+ */
+export class VoterWrapperAdapter {
+  /**
+   * Initializes the class with the given params.
+   *
+   * @constructor
+   * @param {Object} params - An object that contains the initialization params.
+   * - {String} voterId - The unique id of a trustee.
+   */
+  constructor({ voterId }) {
+    this.voterId = voterId;
+    this.wrapper = new VoterWrapper({ voterId });
+  }
+
+  /**
+   * Process the message and update the wrapper status.
+   *
+   * @param {String} messageType - The message type.
+   * @param {Object} decodedData - An object with the data to process.
+   *
+   * @returns {Promise<Object|undefined>}
+   */
+  processMessage(messageType, decodedData) {
+    return this.wrapper.processMessage(messageType, decodedData);
+  }
+
+  /**
+   * Encrypts the plain vote using the wrapper.
+   *
+   * @param {Object} plainVote - An object with the choosen answers for each question.
+   *
+   * @private
+   * @returns {Promise<Object|undefined>}
+   */
+  encrypt(plainVote) {
+    return this.wrapper.encrypt(plainVote);
+  }
+}
