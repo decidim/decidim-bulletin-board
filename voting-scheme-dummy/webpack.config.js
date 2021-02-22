3f48a5bdfb3765c87ca87686d7350c048312714b
David Morcillo <david@codegram.com>
2021-02-22 10:15:08 +0100

    chore: add build script to the voting scheme dummy

---
+++ voting-scheme-dummy/webpack.config.js
@@ -0,0 +1,35 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "dummy-voting-scheme.js",
+    library: "dummyVotingScheme",
+    libraryTarget: "window",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+};
