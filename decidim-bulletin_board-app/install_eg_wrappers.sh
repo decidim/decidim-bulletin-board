041d3fec313093f9f5b1a33f44e117a17fb9d10a
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2021-02-23 17:52:12 +0100

    cleanup the install_eg_wrappers script (#93)
    Co-authored-by: David Morcillo <david@codegram.com>

--- decidim-bulletin_board-app/install_eg_wrappers.sh
+++ decidim-bulletin_board-app/install_eg_wrappers.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Install ElectionGuard dependencies
-sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev
+set -e
 
-# Update pip
+echo "Installing ElectionGuard dependencies"
+sudo apt-get install -y libgmp-dev libmpfr-dev libmpc-dev libffi-dev
+
+echo "Update Pip"
 python3 -m pip install cryptography==3.2.1
 
-# Install Decidim ElectionGuard wrappers (temporarily from the repository)
-rm -rf ../../decidim-electionguard
-git clone https://github.com/decidim/decidim-electionguard.git ../../decidim-electionguard
-cd ../../decidim-electionguard && python3 setup.py install
+echo "Install Decidim ElectionGuard wrappers (temporarily from the repository)"
+if [ -d /tmp/decidim-electionguard ] ; then
+  rm -rf /tmp/decidim-electionguard
+fi
+
+git clone https://github.com/decidim/decidim-electionguard.git /tmp/decidim-electionguard
+cd /tmp/decidim-electionguard
+python3 setup.py install
