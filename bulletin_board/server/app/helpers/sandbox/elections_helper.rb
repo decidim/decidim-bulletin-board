ff02b04cb3287b24110cd5b94fc2a873dac030c8
David Morcillo <david@codegram.com>
2021-04-12 13:51:28 +0200

    feat: add ballot styles to the bulletin board sandbox

---
+++ bulletin_board/server/app/helpers/sandbox/elections_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Sandbox
+  module ElectionsHelper
+    def options_for_ballot_style_select(election)
+      options_for_select(election.manifest[:description][:ballot_styles].map { |ballot_style| ballot_style["object_id"] })
+    end
+
+    def election_questions_with_answers(election)
+      election.manifest[:description][:contests].inject({}) do |acc, contest|
+        acc[contest[:object_id]] = contest[:ballot_selections].map { |ballot_selection| ballot_selection[:object_id] }
+        acc
+      end
+    end
+
+    def ballot_style_classes(question_id)
+      election.manifest[:description][:ballot_styles].map do |ballot_style|
+        if ballot_style["contests"].include?(question_id)
+          ballot_style["object_id"]
+        else
+          ""
+        end
+      end.join(" ")
+    end
+
+    def has_ballot_styles?(election)
+      election.manifest[:description][:ballot_styles].length > 0
+    end
+  end
+end
