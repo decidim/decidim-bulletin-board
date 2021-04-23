94f7baefa00eec7b15fb43672c98e8b4129309b7
David Morcillo <david@codegram.com>
2021-04-23 10:06:45 +0200

    chore: bump to version 0.21.1

--- bulletin_board/ruby-client/lib/decidim/bulletin_board/version.rb
+++ bulletin_board/ruby-client/lib/decidim/bulletin_board/version.rb
@@ -2,6 +2,6 @@
 
 module Decidim
   module BulletinBoard
-    VERSION = "0.21.0"
+    VERSION = "0.21.1"
   end
 end
--- voting_schemes/dummy/ruby-adapter/lib/voting_schemes/dummy/version.rb
+++ voting_schemes/dummy/ruby-adapter/lib/voting_schemes/dummy/version.rb
@@ -2,6 +2,6 @@
 
 module VotingSchemes
   module Dummy
-    VERSION = "0.21.0"
+    VERSION = "0.21.1"
   end
 end
