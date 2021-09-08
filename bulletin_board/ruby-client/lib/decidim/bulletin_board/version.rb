d93bd05727d736b418127bf96cbb8483868f381e
Leonardo Diez <leiodd@gmail.com>
2021-09-08 08:56:50 +0000

    Bump to version 0.22.3

--- bulletin_board/ruby-client/lib/decidim/bulletin_board/version.rb
+++ bulletin_board/ruby-client/lib/decidim/bulletin_board/version.rb
@@ -2,6 +2,6 @@
 
 module Decidim
   module BulletinBoard
-    VERSION = "0.22.2"
+    VERSION = "0.22.3"
   end
 end
--- voting_schemes/dummy/ruby-adapter/lib/voting_schemes/dummy/version.rb
+++ voting_schemes/dummy/ruby-adapter/lib/voting_schemes/dummy/version.rb
@@ -2,6 +2,6 @@
 
 module VotingSchemes
   module Dummy
-    VERSION = "0.22.2"
+    VERSION = "0.22.3"
   end
 end
