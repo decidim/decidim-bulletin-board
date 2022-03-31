70eff86b29888ab6622c2dce13da1b02039a9742
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2022-03-31 16:05:56 +0300

    Fix the disabled Rubocop cop name

--- bulletin_board/server/lib/test/private_keys.rb
+++ bulletin_board/server/lib/test/private_keys.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/LineLength
+# rubocop:disable Layout/LineLength
 
 module Test
   module PrivateKeys
@@ -80,4 +80,4 @@ module Test
   end
 end
 
-# rubocop:enable Metrics/LineLength
+# rubocop:enable Layout/LineLength
